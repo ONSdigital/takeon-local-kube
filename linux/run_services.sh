a2421c931ce2adfc791d3e3d36887d2927be5dfd
RyanKilbane <RyanKilbane92@hotmail.com>
2019-08-28 13:49:39 +0100

    Added linux folder and script

---
+++ linux/run_services.sh
@@ -0,0 +1,147 @@
+#! /bin/bash
+
+usage="$(basename "$0") [-h][-d file] -- script to automatically create our microservices locally
+
+where:
+	-h shows this help text
+	-d database yaml file"
+while getopts ":d:p:b:u:" opt; do
+  case $opt in
+    d) db="$OPTARG"
+    ;;
+    p) pl="$OPTARG"
+    ;;
+    b) bl="$OPTARG"
+	;;
+    u) ui="$OPTARG"
+	;;
+    \?) echo "Invalid option -$OPTARG"  >&2
+	exit 1 
+    ;;
+  esac
+done
+echo $db
+echo $pl
+echo $bl
+echo $ui
+# Start minikube context
+
+minikube start
+eval $(minikube docker-env)
+
+kubectl create -f $db
+
+export DB_SERVER=$(kubectl get pods -o wide | grep "postgres" | awk '{ print $6 }')
+docker build -t takeon-dev-pl $pl
+docker build -t takeon-dev-bl $bl
+docker build -t takeon-dev-ui $ui
+
+# Add service account
+cat << EOF | kubectl apply -f -
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  namespace: default
+  name: service-reader
+rules:
+- apiGroups: [""] # "" indicates the core API group
+  resources: ["services"]
+  verbs: ["get", "watch", "list"]
+EOF
+
+# Create persistence service
+
+cat << EOF | kubectl apply -f -
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: persistence-layer
+  namespace: default
+spec:
+  replicas: 1
+  template:
+    metadata:
+      name: persistence-layer
+      labels:
+        app: persistence-layer
+    spec:
+      containers:
+      - name: persistence-layer
+        env:
+          - name: DATASOURCE_USERNAME
+            valueFrom:
+              configMapKeyRef:
+                name: database-credentials
+                key: datasource_username
+          - name: DATASOURCE_PASSWORD
+            valueFrom:
+              configMapKeyRef:
+                name: database-credentials
+                key: datasource_password
+          - name: DB_SERVER
+            value: $DB_SERVER
+          - name: DB_PORT
+            value: "5432"
+          - name: DB_NAME
+            value: validationdb
+        image: takeon-dev-pl
+        imagePullPolicy: Never
+        ports:
+        - containerPort: 8090
+EOF
+
+kubectl expose deployment persistence-layer --type=LoadBalancer
+
+# Create business service
+cat <<EOF | kubectl apply -f -
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: business-layer
+  namespace: default
+spec:
+  replicas: 1
+  template:
+    metadata:
+      name: business-layer
+      labels:
+        app: business-layer
+    spec:
+      containers:
+      - name: business-layer
+        image: takeon-dev-bl
+        imagePullPolicy: Never
+        ports:
+        - containerPort: 8088
+EOF
+
+kubectl expose deployment business-layer --type=LoadBalancer
+
+# Create UI service
+cat <<EOF | kubectl apply -f -
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: ui-layer
+  namespace: default
+spec:
+  replicas: 1
+  template:
+    metadata:
+      name: ui-layer
+      labels:
+        app: ui-layer
+    spec:
+      containers:
+      - name: ui-layer
+        image: takeon-dev-ui
+        imagePullPolicy: Never
+        ports:
+        - containerPort: 5000
+EOF
+
+kubectl expose deployment ui-layer --type=LoadBalancer
+
+URL=$(minikube service ui-layer --url)
+
+echo "UI URL: $URL"
