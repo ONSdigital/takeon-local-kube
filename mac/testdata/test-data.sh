7454be950a0977f5b00d3b3446eebdec9f6ab50d
ranjit-das <Ranjit.Das@ons.gov.uk>
2019-09-02 14:33:04 +0100

    LU-3422: Create local environment to run all layers on Mac

--- mac/testdata/test-data.sh
+++ mac/testdata/test-data.sh
@@ -5,6 +5,5 @@ export DB_USER=`echo -n ${SECRET_OF_DB_USERNAME} | base64 --decode`
 echo ${POSTGRES_POD}
 echo ${DB_USER}
 echo ${DB_NAME}
-kubectl cp ../env ${POSTGRES_POD}:/tmp
-kubectl cp ./test-data.sql ${POSTGRES_POD}:/tmp
+kubectl cp testdata/test-data.sql ${POSTGRES_POD}:/tmp
 kubectl exec -it ${POSTGRES_POD} -- psql -d ${DB_NAME}  -U ${DB_USER} -p 5432 -f "/tmp/test-data.sql"
