0e75ef4ae8d3aadf10619d4a66895acfcd5c81fa
RyanKilbane <RyanKilbane92@hotmail.com>
2019-09-03 22:27:22 +0100

    removed unneeded entry script in linux directory

--- linux/entry.sh
+++
@@ -1,4 +0,0 @@
-
-su postgres
-postgres psql -f /takeon-db/tables.sql
-postgres psql -d collectiondev -c "CREATE USER g8+CMFFNKKkr9B25RXy/5RAQx5vwdq6LDCCLQY5d6Fw= WITH PASSWORD 'aoBs2HaevDkYjH+QuTGYo3zvNG0KRvyI9TWLk5Oiwow='"
