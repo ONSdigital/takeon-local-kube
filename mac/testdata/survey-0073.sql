4bb354d1f130a9989e1dccbd909995c2ea9c01e4
sufyaniqbal <sufyaniqbal@me.com>
2020-01-03 12:01:58 +0000

    added data to validationform and validationparamemeter for 0073

--- mac/testdata/survey-0073.sql
+++ mac/testdata/survey-0073.sql
@@ -77,69 +77,6 @@ Values
 ( 3, '0145', 'Q145', 'New works brought into use since date of last return', 13, 'NUMERIC', '', 'fisdba', now() ),
 ( 3, '0146', 'Q146', 'Comment on the figures included in your return', 14, 'TICKBOX-Yes', '', 'fisdba', now() );
 
-Insert Into dev01.Contributor
-(
-    Reference                  ,
-    Period                     ,
-    Survey                     ,
-    FormID                     ,
-    Status                     ,
-    ReceiptDate                ,
-    FormType                   ,
-    Checkletter                ,
-    FrozenSicOutdated          ,
-    RuSicOutdated              ,
-    FrozenSic                  ,
-    RuSic                      ,
-    FrozenEmployees            ,
-    Employees                  ,
-    FrozenEmployment           ,
-    Employment                 ,
-    FrozenFteEmployment        ,
-    FteEmployment              ,
-    FrozenTurnover             ,
-    Turnover                   ,
-    EnterpriseReference        ,
-    WowEnterpriseReference     ,
-    CellNumber                 ,
-    Currency                   ,
-    VatReference               ,
-    PayeReference              ,
-    CompanyRegistrationNumber  ,
-    NumberLiveLocalUnits       ,
-    NumberLiveVat              ,
-    NumberLivePaye             ,
-    LegalStatus                ,
-    ReportingUnitMarker        ,
-    Region                     ,
-    BirthDate                  ,
-    EnterpriseName             ,
-    ReferenceName              ,
-    ReferenceAddress           ,
-    ReferencePostcode          ,
-    TradingStyle               ,
-    Contact                    ,
-    Telephone                  ,
-    Fax                        ,
-    SelectionType              ,
-    InclusionExclusion         ,
-    CreatedBy                  ,
-    CreatedDate
-)
-Values
-
-(   '49900138556', '201903', '0073', 3, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900138556', '2967466849', 1, 'S', '326935020502', '2135632144542', '97395797', 178, 0, 0, '1', 'E', 'NP', '20/08/2001', 'Walker-Tucker', 'Jones, Gibbons and Hall', '536 Kaur plaza, East Harry, Wales', 'L2T 0PR', 'Sole Trader', 'Malcolm Bryant', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-(   '49900189484', '201903', '0073', 3, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 25, 25, 22, 22, 22, 22, 12345, 12345, '9900189484', '2905627610', 1, 'S', '423279595848', '9446786207965', '55841012', 71, 0, 0, '1', 'E', 'BS', '16/05/1993', 'Spencer Ltd', 'Quinn Group', '7 Marc shoal, Denisfort, Wales', 'N60 6TL', 'Sole Trader', 'Abdul Stevens', '0117 9555123', '', 'P', '', 'fisdba', now() ),
-(   '49900190211', '201903', '0073', 3, 'Dispatched', now(), '0004', 'F', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900190211', '2981901571', 1, 'S', '979024265812', '3101057635557', '74239110', 2, 0, 0, '1', 'E', 'CF', '08/11/2018', 'Holloway and Sons', 'Hammond and Sons', '14 Ellis passage, Watershaven, Scotland', 'L8 9QE', 'Franchise', 'Sharon Hawkins', '029 20555123', '', 'P', '', 'fisdba', now() ),
-(   '49900190505', '201903', '0073', 3, 'Dispatched', now(), '0004', 'S', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900190505', '2930251417', 1, 'S', '650195077639', '5864180611950', '94403544', 7, 1, 0, '1', 'E', 'CF', '29/06/2005', 'Harvey-Perkins', 'Moran, Wood and Arnold', '690 Ford keys, Kellyhaven, England', 'CM4B 6UR', '', 'Kate ODonnell', '01633 5559912', '', 'P', '', 'fisdba', now() ),
-(   '49900228645', '201903', '0073', 3, 'Dispatched', now(), '0004', 'M', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900228645', '2902674255', 1, 'S', '898038558701', '2690058876978', '69059744', 6, 1, 0, '1', 'E', 'NP', '08/08/2012', 'Khan-Jackson', 'Potter Group', '33 Roberts tunnel, Newport, UK', 'NP10 1AA', '', 'Josephine Reid', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-
-(   '49900138556', '201906', '0073', 3, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900138556', '2967466849', 1, 'S', '326935020502', '2135632144542', '97395797', 178, 0, 0, '1', 'E', 'NP', '20/08/2001', 'Walker-Tucker', 'Jones, Gibbons and Hall', '536 Kaur plaza, East Harry, Wales', 'L2T 0PR', 'Sole Trader', 'Malcolm Bryant', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-(   '49900189484', '201906', '0073', 3, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 25, 25, 22, 22, 22, 22, 12345, 12345, '9900189484', '2905627610', 1, 'S', '423279595848', '9446786207965', '55841012', 71, 0, 0, '1', 'E', 'BS', '16/05/1993', 'Spencer Ltd', 'Quinn Group', '7 Marc shoal, Denisfort, Wales', 'N60 6TL', 'Sole Trader', 'Abdul Stevens', '0117 9555123', '', 'P', '', 'fisdba', now() ),
-(   '49900190211', '201906', '0073', 3, 'Dispatched', now(), '0004', 'F', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900190211', '2981901571', 1, 'S', '979024265812', '3101057635557', '74239110', 2, 0, 0, '1', 'E', 'CF', '08/11/2018', 'Holloway and Sons', 'Hammond and Sons', '14 Ellis passage, Watershaven, Scotland', 'L8 9QE', 'Franchise', 'Sharon Hawkins', '029 20555123', '', 'P', '', 'fisdba', now() ),
-(   '49900190505', '201906', '0073', 3, 'Dispatched', now(), '0004', 'S', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900190505', '2930251417', 1, 'S', '650195077639', '5864180611950', '94403544', 7, 1, 0, '1', 'E', 'CF', '29/06/2005', 'Harvey-Perkins', 'Moran, Wood and Arnold', '690 Ford keys, Kellyhaven, England', 'CM4B 6UR', '', 'Kate ODonnell', '01633 5559912', '', 'P', '', 'fisdba', now() ),
-(   '49900228645', '201906', '0073', 3, 'Dispatched', now(), '0004', 'M', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900228645', '2902674255', 1, 'S', '898038558701', '2690058876978', '69059744', 6, 1, 0, '1', 'E', 'NP', '08/08/2012', 'Khan-Jackson', 'Potter Group', '33 Roberts tunnel, Newport, UK', 'NP10 1AA', '', 'Josephine Reid', '01633 5551234', '', 'P', '', 'fisdba', now() );
-
 
 Insert Into dev01.ValidationForm
 (
@@ -153,10 +90,26 @@ Insert Into dev01.ValidationForm
     CreatedDate
 )
 Values
-    (7310,3,'POPM','0601','0','W',current_user,now()),
-    (7320,3,'POPZC','0602','0','E',current_user,now()),
-    (7330,3,'VP','0147','0','E',current_user,now()),
+    (7310,3,'POPM','0102','0','W',current_user,now()),
+    (7311,3,'POPM','0112','0','W',current_user,now()),
+    (7312,3,'POPM','0103','0','W',current_user,now()),
+    (7313,3,'POPM','0113','0','W',current_user,now()),
+    (7314,3,'POPM','0122','0','W',current_user,now()),
+    (7315,3,'POPM','0123','0','W',current_user,now()),
+
+    (7320,3,'POPZC','0104','0','E',current_user,now()),
+    (7321,3,'POPZC','0104','0','E',current_user,now()),
+    (7322,3,'POPZC','0114','0','E',current_user,now()),
+    (7323,3,'POPZC','0114','0','E',current_user,now()),
+    (7324,3,'POPZC','0124','0','E',current_user,now()),
+    (7325,3,'POPZC','0124','0','E',current_user,now()),
+
+    (7330,3,'VP','0145','0','E',current_user,now()),
+    (7331,3,'VP','0146','0','E',current_user,now()),
+
     (7340,3,'POPQVQ','0101','0','W',current_user,now()),
+    (7341,3,'POPQVQ','0101','0','W',current_user,now()),
+
     (7350,3,'QVDQ','0608','0','W',current_user,now());
 
 
@@ -173,18 +126,67 @@ Insert Into dev01.ValidationParameter
     CreatedDate
 )
 Values
-( 7310, 'Default', 'Default', 'question', '0601', 'response', 0, current_user, now() ),
-( 7310, 'Default', 'Default', 'comparison_question', '0601', 'response', 1, current_user, now() ),
-( 7310, 'Default', 'Default', 'threshold', '20000', '', 0, current_user, now() ),
 
-( 7320, 'Default', 'Default', 'question', '0602', 'response', 0, current_user, now() ),
-( 7320, 'Default', 'Default', 'comparison_question', '0602', 'response', 1, current_user, now() ),
+( 7340, 'Default', 'Default', 'question', '0101', 'response', 0, current_user, now() ),
+( 7340, 'Default', 'Default', 'comparison_question', '0101', 'response', 1, current_user, now() ),
+
+( 7341, 'Default', 'Default', 'question', '0111', 'response', 0, current_user, now() ),
+( 7341, 'Default', 'Default', 'comparison_question', '0111', 'response', 1, current_user, now() ),
+
+( 7310, 'Default', 'Default', 'question', '0102', 'response', 0, current_user, now() ),
+( 7310, 'Default', 'Default', 'comparison_question', '0102', 'response', 1, current_user, now() ),
+( 7310, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7311, 'Default', 'Default', 'question', '0112', 'response', 0, current_user, now() ),
+( 7311, 'Default', 'Default', 'comparison_question', '0112', 'response', 1, current_user, now() ),
+( 7311, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7312, 'Default', 'Default', 'question', '0103', 'response', 0, current_user, now() ),
+( 7312, 'Default', 'Default', 'comparison_question', '0103', 'response', 1, current_user, now() ),
+( 7312, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7313, 'Default', 'Default', 'question', '0113', 'response', 0, current_user, now() ),
+( 7313, 'Default', 'Default', 'comparison_question', '0113', 'response', 1, current_user, now() ),
+( 7313, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7350, 'Default', 'Default', 'question', '0104', 'response', 0, current_user, now() ),
+( 7350, 'Default', 'Default', 'comparison_question', '0104', 'response', 1, current_user, now() ),
+( 7320, 'Default', 'Default', 'question', '0104', 'response', 0, current_user, now() ),
+( 7320, 'Default', 'Default', 'comparison_question', '0104', 'response', 1, current_user, now() ),
 ( 7320, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
+( 7321, 'Default', 'Default', 'question', '0104', 'response', 0, current_user, now() ),
+( 7321, 'Default', 'Default', 'comparison_question', '0104', 'response', 1, current_user, now() ),
+( 7321, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
 
-( 7330, 'Default', 'Default', 'question', '0147', 'response', 0, current_user, now() ),
+( 7351, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7351, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7322, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7322, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7322, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
+( 7323, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7323, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7323, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
 
-( 7340, 'Default', 'Default', 'question', '0608', 'response', 0, current_user, now() ),
-( 7340, 'Default', 'Default', 'comparison_question', '9001', 'response', 0, current_user, now() );
+( 7342, 'Default', 'Default', 'question', '0121', 'response', 0, current_user, now() ),
+( 7342, 'Default', 'Default', 'comparison_question', '0121', 'response', 1, current_user, now() ),
 
-( 7350, 'Default', 'Default', 'question', '0101', 'response', 0, current_user, now() ),
-( 7350, 'Default', 'Default', 'comparison_question', '0101', 'response', 1, current_user, now() );
+( 7314, 'Default', 'Default', 'question', '0122', 'response', 0, current_user, now() ),
+( 7314, 'Default', 'Default', 'comparison_question', '0122', 'response', 1, current_user, now() ),
+( 7314, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7315, 'Default', 'Default', 'question', '0123', 'response', 0, current_user, now() ),
+( 7315, 'Default', 'Default', 'comparison_question', '0123', 'response', 1, current_user, now() ),
+( 7315, 'Default', 'Default', 'threshold', '10000', '', 0, current_user, now() ),
+
+( 7352, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7352, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7324, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7324, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7324, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
+( 7325, 'Default', 'Default', 'question', '0114', 'response', 0, current_user, now() ),
+( 7325, 'Default', 'Default', 'comparison_question', '0114', 'response', 1, current_user, now() ),
+( 7325, 'Default', 'Default', 'threshold', '0', '', 0, current_user, now() ),
+
+( 7330, 'Default', 'Default', 'question', '0145', 'response', 0, current_user, now() ),
+
+( 7331, 'Default', 'Default', 'question', '0146', 'response', 0, current_user, now() );
--- mac/testdata/survey-0074.sql
+++ mac/testdata/survey-0074.sql
@@ -85,69 +85,6 @@ Values
 ( 4, '0145', 'Q145', 'New works brought into use since date of last return', 17, 'NUMERIC', '', 'fisdba', now() ),
 ( 4, '0146', 'Q146', 'Comment on the figures included in your return', 18, 'TICKBOX-Yes', '', 'fisdba', now() );
 
-Insert Into dev01.Contributor
-(
-    Reference                  ,
-    Period                     ,
-    Survey                     ,
-    FormID                     ,
-    Status                     ,
-    ReceiptDate                ,
-    FormType                   ,
-    Checkletter                ,
-    FrozenSicOutdated          ,
-    RuSicOutdated              ,
-    FrozenSic                  ,
-    RuSic                      ,
-    FrozenEmployees            ,
-    Employees                  ,
-    FrozenEmployment           ,
-    Employment                 ,
-    FrozenFteEmployment        ,
-    FteEmployment              ,
-    FrozenTurnover             ,
-    Turnover                   ,
-    EnterpriseReference        ,
-    WowEnterpriseReference     ,
-    CellNumber                 ,
-    Currency                   ,
-    VatReference               ,
-    PayeReference              ,
-    CompanyRegistrationNumber  ,
-    NumberLiveLocalUnits       ,
-    NumberLiveVat              ,
-    NumberLivePaye             ,
-    LegalStatus                ,
-    ReportingUnitMarker        ,
-    Region                     ,
-    BirthDate                  ,
-    EnterpriseName             ,
-    ReferenceName              ,
-    ReferenceAddress           ,
-    ReferencePostcode          ,
-    TradingStyle               ,
-    Contact                    ,
-    Telephone                  ,
-    Fax                        ,
-    SelectionType              ,
-    InclusionExclusion         ,
-    CreatedBy                  ,
-    CreatedDate
-)
-Values
-
-(   '49900229065', '201903', '0074', 4, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900229065', '2993511516', 1, 'S', '326935020502', '2135632144542', '97395797', 178, 0, 0, '1', 'E', 'NP', '07/08/2019', 'Jones, Banks and Hall', 'Bell LLC', '9 Thompson drive, East Harry, Wales', 'L2T 0PR', 'Sole Trader', 'Dennis Edwards', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-(   '49900356828', '201903', '0074', 4, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 25, 25, 22, 22, 22, 22, 12345, 12345, '9900356828', '2921377360', 1, 'S', '423279595848', '9446786207965', '55841012', 71, 0, 0, '1', 'E', 'BS', '21/03/2019', 'Hunter, Coles and Turnbull', 'Savage, Parkinson and Wright', '526 Amber estates, Denisfort, Wales', 'N60 6TL', 'Sole Trader', 'Jacob Thorpe', '0117 9555123', '', 'P', '', 'fisdba', now() ),
-(   '49900423293', '201903', '0074', 4, 'Dispatched', now(), '0004', 'F', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900423293', '2962684174', 1, 'S', '979024265812', '3101057635557', '74239110', 2, 0, 0, '1', 'E', 'CF', '06/09/1995', 'Curtis LLC', 'Ford PLC', '36 Giles corners, Watershaven, Scotland', 'L8 9QE', 'Franchise', 'Gillian Bailey', '029 20555123', '', 'P', '', 'fisdba', now() ),
-(   '49900423920', '201903', '0074', 4, 'Dispatched', now(), '0004', 'S', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900423920', '2993702519', 1, 'S', '650195077639', '5864180611950', '94403544', 7, 1, 0, '1', 'E', 'CF', '19/03/1973', 'Wilson-Chapman', 'Jones LLC', '2 Brown keys, Kellyhaven, England', 'CM4B 6UR', '', 'Anthony Wilson', '01633 5559912', '', 'P', '', 'fisdba', now() ),
-(   '49900449878', '201903', '0074', 4, 'Dispatched', now(), '0004', 'M', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900449878', '2910550332', 1, 'S', '898038558701', '2690058876978', '69059744', 6, 1, 0, '1', 'E', 'NP', '31/01/1977', 'Barrett Group', 'Hutchinson, Daly and Pritchard', '17 Evans junction, Newport, UK', 'NP10 1AA', '', 'Arthur Knowles-Gallagher', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-
-(   '49900229065', '201906', '0074', 4, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900229065', '2993511516', 1, 'S', '326935020502', '2135632144542', '97395797', 178, 0, 0, '1', 'E', 'NP', '07/08/2019', 'Jones, Banks and Hall', 'Bell LLC', '9 Thompson drive, East Harry, Wales', 'L2T 0PR', 'Sole Trader', 'Dennis Edwards', '01633 5551234', '', 'P', '', 'fisdba', now() ),
-(   '49900356828', '201906', '0074', 4, 'Dispatched', now(), '0004', 'T', '70110', '70110', '41100', '41100', 25, 25, 22, 22, 22, 22, 12345, 12345, '9900356828', '2921377360', 1, 'S', '423279595848', '9446786207965', '55841012', 71, 0, 0, '1', 'E', 'BS', '21/03/2019', 'Hunter, Coles and Turnbull', 'Savage, Parkinson and Wright', '526 Amber estates, Denisfort, Wales', 'N60 6TL', 'Sole Trader', 'Jacob Thorpe', '0117 9555123', '', 'P', '', 'fisdba', now() ),
-(   '49900423293', '201906', '0074', 4, 'Dispatched', now(), '0004', 'F', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900423293', '2962684174', 1, 'S', '979024265812', '3101057635557', '74239110', 2, 0, 0, '1', 'E', 'CF', '06/09/1995', 'Curtis LLC', 'Ford PLC', '36 Giles corners, Watershaven, Scotland', 'L8 9QE', 'Franchise', 'Gillian Bailey', '029 20555123', '', 'P', '', 'fisdba', now() ),
-(   '49900423920', '201906', '0074', 4, 'Dispatched', now(), '0004', 'S', '70110', '70110', '41100', '41100', 111, 1112, 100, 100, 100, 100, 99999, 99999, '9900423920', '2993702519', 1, 'S', '650195077639', '5864180611950', '94403544', 7, 1, 0, '1', 'E', 'CF', '19/03/1973', 'Wilson-Chapman', 'Jones LLC', '2 Brown keys, Kellyhaven, England', 'CM4B 6UR', '', 'Anthony Wilson', '01633 5559912', '', 'P', '', 'fisdba', now() ),
-(   '49900449878', '201906', '0074', 4, 'Dispatched', now(), '0004', 'M', '70110', '70110', '41100', '41100', 750, 748, 100, 100, 100, 100, 99999, 99999, '9900449878', '2910550332', 1, 'S', '898038558701', '2690058876978', '69059744', 6, 1, 0, '1', 'E', 'NP', '31/01/1977', 'Barrett Group', 'Hutchinson, Daly and Pritchard', '17 Evans junction, Newport, UK', 'NP10 1AA', '', 'Arthur Knowles-Gallagher', '01633 5551234', '', 'P', '', 'fisdba', now() );
-
 
 Insert Into dev01.ValidationForm
 (
